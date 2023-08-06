0698ed939fdc1bbb094aabf3d0a24fa6ed448768
Lemonclff <z860844717z@gmail.com>
2023-08-06 13:19:34 +0800

    frontend css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
+import RegistrationForm from './pages/RegistrationForm';
 import './style/tailwind.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
+        <Route path="/Registration" element={<RegistrationForm/>} />
       </Routes>
       </BrowserRouter>
     </div>
